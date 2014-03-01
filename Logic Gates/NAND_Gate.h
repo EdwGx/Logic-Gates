c90f9e28afcf8ea00353939b44ff666a432e8565
EdwGx <edguo98@gmail.com>
2014-03-01 15:31:24 -0500

    ADD NAND Gate class

---
+++ Logic Gates/NAND_Gate.h
@@ -0,0 +1,13 @@
+//
+//  NAND_Gate.h
+//  Logic Gates
+//
+//  Created by edguo on 3/1/2014.
+//  Copyright (c) 2014 Edward Peiliang Guo. All rights reserved.
+//
+
+#import "Gates.h"
+
+@interface NAND_Gate : Gates
+
+@end
