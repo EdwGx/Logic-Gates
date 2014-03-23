25fc9166c64b0a27a0d0f3dfe7be55a59ead0ff5
EdwGx <edguo98@gmail.com>
2014-03-23 08:32:18 -0400

    More codes with save/load/remove

--- Logic Gates/MapFileScene.m
+++ Logic Gates/MapFileScene.m
@@ -80,13 +80,44 @@
 }
 
 -(void)buttonTouchUp:(NSUInteger)index{
-    if (index>0) {
-        for (ButtonSprite*button in self.buttonMap.children) {
-            if (button.button_id>index) {
-                SKAction* action = [SKAction moveByX:0 y:50 duration:0.2];
-                [button runAction:action];
-            }
+    ButtonSprite* newSelectButton;
+    
+    for (ButtonSprite*button in self.buttonMap.children) {
+        if (button.button_id>index && button.button_id<=self.selectButton.button_id) {
+            SKAction* action = [SKAction moveByX:0 y:50 duration:0.2];
+            [button runAction:action];
+        } else if (button.button_id<=index && button.button_id>self.selectButton.button_id){
+            SKAction* action = [SKAction moveByX:0 y:-50 duration:0.2];
+            [button runAction:action];
         }
+        if (button.button_id == index) {
+            newSelectButton = button;
+        }
+    }
+    if (index != 0) {
+        self.selectButton = newSelectButton;
+        
+        CGFloat posX = self.selectButton.position.x - self.selectButton.size.width/2 + 25;
+        CGFloat posY = self.selectButton.position.y - self.selectButton.size.height/2 - 20;
+        
+        SKSpriteNode* saveButton = [SKSpriteNode spriteNodeWithImageNamed:@"save_button"];
+        saveButton.name = @"save";
+        saveButton.position = CGPointMake(posX, posY);
+        [self addChild:saveButton];
+        
+        posX += 60;
+        SKSpriteNode* loadButton = [SKSpriteNode spriteNodeWithImageNamed:@"load_button"];
+        loadButton.name = @"load";
+        loadButton.position = CGPointMake(posX, posY);
+        [self addChild:loadButton];
+        
+        posX += 60;
+        SKSpriteNode* removeButton = [SKSpriteNode spriteNodeWithImageNamed:@"remove_button"];
+        removeButton.name = @"remove";
+        removeButton.position = CGPointMake(posX, posY);
+        [self addChild:removeButton];
+    }else{
+        self.selectButton = nil;
     }
 }
 
@@ -111,6 +142,19 @@
     }
 }
 
+-(void)saveMapWithFileName:(NSString*)name{
+    [mMap performSelectorInBackground:@selector(saveMap:) withObject:name];
+}
+
+-(void)loadMapWithFileName:(NSString*)name{
+    [mMap killAllGates];
+    [mMap performSelectorInBackground:@selector(saveMap:) withObject:name];
+}
+
+-(void)removeMapWithFileName:(NSString*)name{
+    [mMap performSelectorInBackground:@selector(removeMapFile:) withObject:name];
+}
+
 -(void)handleTapFrom:(UITapGestureRecognizer*)recognizer{
     if (recognizer.state == UIGestureRecognizerStateEnded) {
         CGPoint pointInScene = [self convertPointFromView:[recognizer locationInView:self.view]];
@@ -127,9 +171,28 @@
                     [self buttonTouchUp:button.button_id];
                 }
             } else if ([node isEqual:self.addButton]){
+                
                 [self setupAddAlertView];
+                
             } else if ([node isEqual:self.backButton]){
+                
                 [self presentMainScene];
+                
+            } else if ([node.name isEqualToString:@"save"] && self.selectButton){
+                
+                NSString*name = [mMap.filesList objectAtIndex:self.selectButton.button_id];
+                [self saveMapWithFileName:name];
+                
+            } else if ([node.name isEqualToString:@"load"] && self.selectButton){
+                
+                NSString*name = [mMap.filesList objectAtIndex:self.selectButton.button_id];
+                [self loadMapWithFileName:name];
+                
+            } else if ([node.name isEqualToString:@"remove"] && self.selectButton){
+                
+                NSString*name = [mMap.filesList objectAtIndex:self.selectButton.button_id];
+                [self removeMapWithFileName:name];
+                
             }
         }
         
